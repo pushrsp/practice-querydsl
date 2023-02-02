ef3d95de883a2de5fcf8bb78af55f062dd84adea
PUSHRSP <pushrsp@gmail.com>
2023-02-02 12:49:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/practicequerydsl/PracticeQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicequerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeQuerydslApplication.class, args);
+    }
+
+}
